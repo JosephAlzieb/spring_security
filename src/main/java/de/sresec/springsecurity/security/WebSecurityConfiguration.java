fecd550ad8b4cfef40ae59d8112d97a5a0d3fe6d
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-01 12:47:57 +0200

    auf index Seite sollte man ohne Anmeldung zugreifen können

--- src/main/java/de/sresec/springsecurity/security/WebSecurityConfiguration.java
+++ src/main/java/de/sresec/springsecurity/security/WebSecurityConfiguration.java
@@ -1,18 +1,34 @@
 package de.sresec.springsecurity.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+  private PasswordEncoder passwordEncoder;
+
+  @Autowired
+  public WebSecurityConfiguration(PasswordEncoder passwordEncoder) {
+    this.passwordEncoder = passwordEncoder;
+  }
+
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http
         .authorizeRequests()
+        .antMatchers("/","/error", "/css/**", "/img/**")
+        .permitAll()
         .anyRequest()
         .authenticated()
         .and()
