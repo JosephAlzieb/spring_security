0a9f88cd4565f4b5652049820c00c963f1b3eb50
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-01 12:13:44 +0200

    rest controller für student

---
+++ src/main/java/de/sresec/springsecurity/controllers/StudentController.java
@@ -0,0 +1,29 @@
+package de.sresec.springsecurity.controllers;
+
+import de.sresec.springsecurity.student.Student;
+import java.util.List;
+import java.util.NoSuchElementException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+
+  private final static List<Student> STUDENTS = List.of(
+      new Student(1L,"Joe Fess"),
+      new Student(2L,"Geo Famo"),
+      new Student(3L,"Jas Frem"),
+      new Student(4L,"Dan Fess")
+  );
+
+  @GetMapping("{studentid}")
+  public Student getStudent(@PathVariable("studentid") Long id){
+    return STUDENTS.stream()
+        .filter(s -> s.getId().equals(id))
+        .findFirst()
+        .orElseThrow(()-> new NoSuchElementException("Es existiert keinen Student mit dem Id -- "+ id));
+  }
+}
