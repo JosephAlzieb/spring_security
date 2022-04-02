88f77b8f7184161b1dd3f8efc6e27afc7f41b27d
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-02 12:29:23 +0200

    Students Permissions reduziert

--- src/main/java/de/sresec/springsecurity/security/Role.java
+++ src/main/java/de/sresec/springsecurity/security/Role.java
@@ -2,13 +2,15 @@ package de.sresec.springsecurity.security;
 
 import static de.sresec.springsecurity.security.Permission.COURSE_READ;
 import static de.sresec.springsecurity.security.Permission.COURSE_WRITE;
+import static de.sresec.springsecurity.security.Permission.STUDENT_READ;
+import static de.sresec.springsecurity.security.Permission.STUDENT_WRITE;
 
 import com.google.common.collect.Sets;
 import java.util.Set;
 
 public enum Role {
-  STUDENT(Sets.newHashSet(COURSE_READ)),
-  ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE));
+  STUDENT(Sets.newHashSet()),
+  ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE,STUDENT_READ,STUDENT_WRITE));
 
   private final Set<Permission> permissions;
 
