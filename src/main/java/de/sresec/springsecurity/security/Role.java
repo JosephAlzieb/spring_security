f434b8082a4bdcc1bcb503ceeb1b595463c3213f
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-02 12:47:25 +0200

    permissions in GrantedAuthority umgewandelt

--- src/main/java/de/sresec/springsecurity/security/Role.java
+++ src/main/java/de/sresec/springsecurity/security/Role.java
@@ -7,6 +7,8 @@ import static de.sresec.springsecurity.security.Permission.STUDENT_WRITE;
 
 import com.google.common.collect.Sets;
 import java.util.Set;
+import java.util.stream.Collectors;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
 public enum Role {
   STUDENT(Sets.newHashSet()),
@@ -18,7 +20,17 @@ public enum Role {
     this.permissions = permissions;
   }
 
-  public Set<Permission> getPermissions() {
+  private Set<Permission> getPermissions() {
     return permissions;
+  }
+
+  public Set<SimpleGrantedAuthority> getGrantedAuthorities(){
+    Set<SimpleGrantedAuthority> grantedAuthorities = getPermissions().stream()
+        .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+        .collect(Collectors.toSet());
+
+    grantedAuthorities.add(new SimpleGrantedAuthority("ROLE_" + this.name()));
+
+    return grantedAuthorities;
   }
 }
