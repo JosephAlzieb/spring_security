514159d8b77b2471674c92297c7c2b4568ac6853
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-02 11:56:50 +0200

    RequestMapping geändert

--- src/main/java/de/sresec/springsecurity/controllers/StudentController.java
+++ src/main/java/de/sresec/springsecurity/controllers/StudentController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("api/student")
 public class StudentController {
 
   private final static List<Student> STUDENTS = List.of(
