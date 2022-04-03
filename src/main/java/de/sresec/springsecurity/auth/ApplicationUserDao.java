2bf56b4c4815f7870761fc398a6fa4dd8f16f34d
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2022-04-03 14:13:14 +0200

    Dao Interface hinzugefügt

---
+++ src/main/java/de/sresec/springsecurity/auth/ApplicationUserDao.java
@@ -0,0 +1,9 @@
+package de.sresec.springsecurity.auth;
+
+import java.util.Optional;
+
+public interface ApplicationUserDao {
+
+  Optional<ApplicationUser> selectApplicationUserByUsername(String username);
+
+}
